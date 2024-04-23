89aa8a3e9205a1f515802059dbcf0dec5c5f131a
Pinky <ishratjahanpinky2@gmail.com>
2024-04-24 01:12:55 +0600

    added 3d assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, {Suspense} from "react";
 import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader.jsx";
 
 const Home = () => {
   return (
@@ -8,8 +9,15 @@ const Home = () => {
       {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
         popup
       </div> */}
-      <Canvas className="w-full h-screen bg-transparent">
-
+      <Canvas className="w-full h-screen bg-transparent"
+      camera={{ near:0.1, far:1000 }}>
+<Suspense fallback={<Loader/> }>
+<directionalLight/>
+    <ambientLight/>
+    <pointLight/>
+    <spotLight/>
+    <hemisphereLight/>
+</Suspense>
       </Canvas>
     </section>
   );
